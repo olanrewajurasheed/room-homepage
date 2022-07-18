3511899cd31c0f380efbadd53564e39bac4a9514
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-07-18 10:20:04 +0100

    styling continua

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["public/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     screens: {
       sm: '480px',
