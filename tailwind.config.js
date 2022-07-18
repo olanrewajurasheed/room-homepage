25df96f9ef902c44052f7b8edf8244df625df689
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-07-18 02:11:43 +0100

    desktop styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        'darkGray': 'hsl(0, 0%, 63%)',
+        'black': 'hsl(0, 0%, 0%)',
+        'white': 'hsl(0, 0%, 100%)',
+        'veryDarkGray': 'hsl(0, 0%, 27%)'
+      },
+    },
   },
   plugins: [],
 }
