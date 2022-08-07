82e9833887b28907ce843c34e885035eeb24132c
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-08-07 16:09:50 +0100

    Slider for toggling hero section page

--- public/app.js
+++ public/app.js
@@ -27,3 +27,59 @@ function closeNavBarTwo() {
     navbar.style.display = "none";
   }
 }
+
+// Images and dynamic through sliding
+desktop_image = document.getElementById("desktop_image");
+header_text = document.getElementById("header_text");
+
+desktop_images = [
+  "desktop-image-hero-1.jpg",
+  "desktop-image-hero-2.jpg",
+  "desktop-image-hero-3.jpg",
+];
+
+header_texts = [
+  "discover innovative way to decorate",
+  "we are available all across the globe",
+  "Manufactured with the best materials ",
+];
+
+// functions to activate sliding of desktop_images
+let i = 0;
+let j = 0;
+
+function prev() {
+  if (i <= 0) {
+    i = desktop_images.length;
+    i--;
+    return setImg();
+  }
+
+  if (j <= 0) {
+    j = header_texts.length;
+    j--;
+    return setHeader();
+  }
+}
+
+function next() {
+  if (i >= desktop_images.length - 1) {
+    i = -1;
+    i++;
+    return setImg();
+  }
+
+  if (j >= header_texts.length - 1) {
+    j = -1;
+    j++;
+    return setHeader();
+  }
+}
+
+function setImg() {
+  return desktop_image.setAttribute("src", "images/" + desktop_images[i]);
+}
+
+function setHeader() {
+  return header_text.textContent = header_texts[i];
+}
